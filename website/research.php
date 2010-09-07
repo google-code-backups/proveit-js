b23a49127a9184e060eb8d1433d132a0f99c69a3
Matthew Flaschen <matthew.flaschen@gatech.edu>
2010-09-07 15:53:00 -0400

    Remove _notes and Scraps directories.

---
+++ website/research.php
@@ -0,0 +1,73 @@
+<?php
+
+$page = 'Research';
+
+include_once 'header.php';
+
+?>
+
+			<!-- Main Content Table -->
+
+				<div id="leadInPic"><img src="img/research_leadIn.jpg" alt="doc_banner" /></div>
+
+				<table width="960" border="0" cellspacing="0" cellpadding="0" style="background-color: #2682c7; ">
+					<tr>
+						<td style="width: 686px;">
+
+							<div id="mainPic" class="research">
+								<h1>From the Lab</h1>
+							</div><!-- end #mainPic -->
+							<div id="mainBody"> 
+								<p>ProveIt is an open-source Wikipedia referencing tool.  Quisque et leo ac lacus faucibus pellentesque et ac dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales adipiscing sem at viverra. Cum sociis natoque penatibus et magnis dis parturient montes, </p>
+
+								<h3>The Story of ProveIt</h3>
+								<h4>Wikipedia Citations are Important</h4>
+								<p>ProveIt is an open-source Wikipedia referencing tool.  Quisque et leo ac lacus faucibus pellentesque et ac dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales adipiscing sem at viverra. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Curabitur dolor risus, hendrerit eget viverra a, pellentesque et elit. Suspendisse potenti. Duis eget semper leo. Cras dapibus, diam eu semper ullamcorper, augue leo mattis lorem, id porta lacus quam ut odio. Sed vehicula sem vitae sem mattis vehicula. Duis felis diam, vestibulum nec interdum sit amet, ultricies at libero.</p>
+								<h4>Creating Citations are Cumbersome</h4>
+								<p>ProveIt is an open-source Wikipedia referencing tool.  Quisque et leo ac lacus faucibus pellentesque et ac dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales adipiscing sem at viverra. Cum sociis natoque penatibus et magnis dis parturient montes, </p>
+								
+								<h3>Plan of Action</h3>
+								<h4>Simplify the Process</h4>
+								<p>ProveIt is an open-source Wikipedia referencing tool.  Quisque et leo ac lacus faucibus pellentesque et ac dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales adipiscing sem at viverra. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Curabitur dolor risus, hendrerit eget viverra a, pellentesque et elit. Suspendisse potenti. Duis eget semper leo. Cras dapibus, diam eu semper ullamcorper, augue leo mattis lorem, id porta lacus quam ut odio. Sed vehicula sem vitae sem mattis vehicula. Duis felis diam, vestibulum nec interdum sit amet, ultricies at libero.</p>														
+								<!--table>      
+									<tr>
+										<td style="background-color: #2682c7; ">
+											<div id="recommendationHeader"> RECOMMENDED TUTORIALS </div> 
+											<div class="recommend left"> <div class="recommendText firstLine"> Getting Started with</div>
+											<div class="recommendText secondLine"><a href="" class="recommendLink"> ProveIt</a></div></div> 
+		
+											<div class="recommend right"> <div class="recommendText firstLine"> Quick Interface </div>
+											<div class="recommendText secondLine"> <a href="features.html" class="recommendLink"> WALKTHROUGH </a></div></div>
+										</td>
+									</tr>
+								</table -->     
+							</div><!-- end #mainBody -->
+						</td>
+						<td> 
+							<h2> Publications</h2>
+							<div id="sideTableofContents">
+								<h3 class="first">GT Conference</h3>
+								<ul>
+									<li><a href="#"> Paper 1 Link </a></li>
+									<li><a href="#"> Paper 2 Link</a></li>
+								</ul>
+								
+								<h3> Web Press</h3>
+								<ul>
+									<li><a href="#">External Link #1</a></li>
+									<li><a href="#">External Link #2 </a></li>
+									<li><a href="#">External Link #3 </a></li>
+								</ul>
+								
+								<h3> Documents</h3>
+								<ul>
+									<li><a href="#">Item with title #1</a></li>
+									<li><a href="#">Item with title #2</a></li>
+								</ul>
+							</div>
+					  </td>
+					</tr>
+				</table> 
+
+
+<?php include_once 'footer.php'; ?>
