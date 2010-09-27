def935b1ce8bb94afea7e5765725edb998025179
Matthew Flaschen <matthew.flaschen@gatech.edu>
2010-09-26 23:11:16 -0400

    Add support for raw references (references that do not use a template); issue 24

---
+++ website/footer.php
@@ -0,0 +1,41 @@
+				<div class="borderLine footer"></div> 
+		
+				<div id="footerContainer"> 
+					<table width="960" border="0" cellspacing="0" cellpadding="0" style="color: #fff; ">
+						<tr>
+							<!-- Footer Logo -->
+							<td style="width: 150px;"> <a href="index.php" > <img src="img/proveit_logo_small.png" alt="ProveIt logo" /> </a>
+							</td>
+							
+							<!-- Footer Navigation -->
+							<td> 
+								<div id="footerNav">
+									<span style="color:#FFF; padding-right:10px;"> USERS</span>
+									<a href="features.php" class="footerNavLink <?php if($page == 'Features') echo ' selected'; ?>">Features</a> -
+									<a href="screenshots.php" class="footerNavLink <?php if($page == 'Screenshots') echo ' selected'; ?>">Screenshots</a> -
+									<a href="tutorials.php" class="footerNavLink<?php if($page == 'Tutorials') echo ' selected'; ?>">Tutorials</a> -
+									<a href="http://code.google.com/p/proveit-js/issues/entry" class="footerNavLink">Report a Bug</a> -
+				                                        <a href="demo.php" class="footerNavLink<?php if($page == 'Demo') echo ' selected'; ?>">Demo</a>
+									
+									<br/><span style="color:#FFF; padding-right:10px;">DEVELOPERS </span>
+									<a href="documentation.php" class="footerNavLink <?php if($page == 'Documentation') echo ' selected'; ?>">Documentation</a> -			
+									<a href="http://code.google.com/p/proveit-js/" class="footerNavLink">Google Code Home </a> -
+									<a href="" class="footerNavLink"> Wiki </a> 
+									   
+									<br/><span style="color:#FFF; padding-right:10px;">ABOUT US </span>
+									<a href="research.php" class="footerNavLink <?php if($page == 'Research') echo ' selected'; ?>">Research </a> -
+									<a href="theteam.php" class="footerNavLink <?php if($page == 'The Team') echo ' selected'; ?>">The Team</a>
+								</div>
+							</td>
+		
+							<!-- Footer Credits -->
+							<td style="width: 274px;"> 
+								<div id="footerCredits"> <a href="" class="footerLink"> Georgia Tech | Interactive Computing</a><br/> 
+								<a href="" class="footerLink"><span style="color:#fddb00"> Electronic Learning Communities Lab </span></a></div> 
+							</td>
+						</tr>
+					</table>
+				</div>
+</div><!-- end #superContainer -->
+</body>
+</html>
