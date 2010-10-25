d0ec7678f6ac3710e52722b0f67f0bfab2163362
Terris Johnson <tjohnson3@mail.gatech.edu>
2010-10-25 17:11:51 -0400

    - Visual Tweaks and Edits (Missing Images, Logo, Get ProveIt Edits) - Additional Content on HomePage

---
+++ website/theteam.php
@@ -0,0 +1,81 @@
+<?php
+
+$page = 'The Team';
+
+include_once 'header.php';
+
+?>
+
+			<!-- Main Content Table -->
+
+				<table id="mainTable">
+					<tr>
+						<td id="mainContent">
+
+							<div id="mainPic" class="theteam">
+								<h1>The Team</h1>
+							</div><!-- end #mainPic -->
+							<div id="mainBody"> 
+								<p>ProveIt has been in development since 2008 and is a highly collaborative effort. These are some of the folks who have helped out with this project since it began.</p>
+								
+								<h2>Current Team Members</h2>
+								
+								<h3>Kurt Luther (Lead Researcher &amp; Project Manager)</h3>
+								<p>Kurt is a 5th-year Ph.D. candidate in social computing at Georgia Tech. His dissertation work examines the role of leadership in online creative collaboration. He has been editing the English language Wikipedia since December 2005 under the username <a href="http://en.wikipedia.org/wiki/User:MaxVeers">MaxVeers</a>. He was a primary contributor to two Featured Articles, <a href="http://en.wikipedia.org/wiki/Tech_Tower">Tech Tower</a> and <a href="http://en.wikipedia.org/wiki/ANAK_Society">ANAK Society</a>, both of which have been featured on Wikipedia's home page. In 2010 he received a <a href="http://en.wikipedia.org/wiki/Wikipedia:Four_Award">Four Award</a> for his work on the ANAK Society article. He is also a founding member of <a href="http://en.wikipedia.org/wiki/Wikipedia:WikiProject_Georgia_Tech">WikiProject Georgia Tech</a>.</p>
+								
+								<h3>Matthew Flaschen (Lead Developer) </h3>
+								<p>bio goes here</p>										
+
+								<h3>Terris Johnson (Lead Designer) </h3>
+								<p>Terris is a 5th-year Computational Media student at Georgia Tech. He was born in Louisville, Kentucky and moved to Atlanta, GA at the age of 12. Coming from a background in visual design and a deep interest in psychology, Terris' interest in computing focuses on user-interface and user-experience design. He has been involved with undergraduate research for over 4 years. Terris joined the ProveIt team as user-interface and website designer hoping to help deliver a product that is not only functional, but also beautiful.</p>									
+								<h3>Prof. Amy Bruckman</h3>
+								<p>Amy is an associate professor at Georgia Tech.</p>
+								
+								<h2>Former Team Members</h2>
+								
+								<h3>Prof. Andrea Forte</h3>
+								<p>Andrea received her Ph.D. in human-centered computing from Georgia Tech in 2009 and is now an assistant professor at Drexel University.</p>
+								
+								<h3>Christopher Jordan</h3>
+								<p>Christopher received his B.S. from Georgia Tech.</p>
+							</div><!-- end #mainBody -->
+						</td>
+						<td id="sideTableofContents"> 
+							<h2> More About Us</h2>
+								<h3 class="first">Kurt Luther</h3>
+								<ul>
+									<li><a href="http://www.kurtluther.com/"> Kurt's website</a></li>
+								</ul>
+								
+								<h3> Matthew Flaschen</h3>
+								<ul>
+									<li><a href="#">Matt's website</a></li>
+								</ul>
+								
+								<h3> Terris Johnson </h3>
+								<ul>
+									<li><a href="#">Terris' website</a></li>
+								</ul>
+								
+								<h3> Prof. Amy Bruckman </h3>
+								<ul>
+									<li><a href="http://www.cc.gatech.edu/~asb/">Amy's website</a></li>
+								</ul>
+
+								<h3> Prof. Andrea Forte </h3>
+								<ul>
+									<li><a href="http://www.andreaforte.net/">Andrea's website</a></li>
+								</ul>
+								
+								<h3> Christopher Jordan </h3>
+								<ul>
+									<li><a href="#">Chris' website</a></li>
+								</ul>								
+								
+							
+					  </td>
+					</tr>
+				</table> 
+
+
+<?php include_once 'footer.php'; ?>
