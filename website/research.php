1004166f4d336d37ebd050421c848b8e3de6323c
luther <luther@localhost>
2010-10-26 12:08:16 -0400

    * reorder About page links to match header/footer * Wiki links go directly to Google Code wiki * removed extra spaces in footer links

---
+++ website/research.php
@@ -0,0 +1,41 @@
+<?php
+
+$page = 'Research';
+
+include_once 'header.php';
+
+?>
+
+			<!-- Main Content Table -->
+
+				<table id="mainTable">
+					<tr>
+						<td id="mainContent">
+
+							<div id="mainPic" class="research">
+								<h1>Research</h1>
+							</div><!-- end #mainPic -->
+							<div id="mainBody"> 
+							
+								<h2>Demonstrations</h2>
+								<p>We have demoed ProveIt at the following symposium:</p>
+								
+								<ul>
+									<li>Kurt Luther, Matthew Flaschen, Andrea Forte, Christopher Jordan, and Amy Bruckman, <strong><a href="/pdf/a43-luther.pdf">ProveIt: A New Tool for Supporting Citation in MediaWiki</a></strong>, presented at the 2009 ACM Symposium on Wikis and Open Collaboration (WikiSym '09), Orlando, FL</li>
+								</ul>
+								
+								<h2>Posters</h2>
+								<p>We have done poster presentations of ProveIt at the following symposia:</p>
+								<ul>
+									<li>Matt Flaschen, Christopher Jordan, Kurt Luther, Andrea Forte, and Amy Bruckman, <strong>ProveIt: A New Tool for Reliable Wikipedia References</strong>, presented at the 2009 Georgia Tech Undergraduate Research in Computing (UROC) Symposium, Atlanta, GA</li>
+									<li>Matt Flaschen, Christopher Jordan, Kurt Luther, Andrea Forte, and Amy Bruckman, <strong>ProveIt: A New Tool for Reliable Wikipedia References</strong>, presented at the 2009 Georgia Tech Undergraduate Research (UROP) Spring Symposium, Atlanta, GA</li>
+								</ul>
+   
+							</div><!-- end #mainBody -->
+						</td>
+						<td id="sideTableofContents">&nbsp; </td>
+					</tr>
+				</table> 
+
+
+<?php include_once 'footer.php'; ?>
