ef6c292ae7c6c9008cc6e133438b7c6b3d19e2d9
Matthew Flaschen <matthew.flaschen@gatech.edu>
2010-11-23 15:51:02 -0500

    Added tag release2.0 for changeset a73cc9a01c4e

---
+++ website/research.php
@@ -0,0 +1,43 @@
+<?php
+
+$page = 'Research';
+
+include_once 'header.php';
+
+?>
+
+			<!-- Main Content Table -->
+
+				<table id="mainTable">
+					<tr>
+						<td id="mainContent">
+
+							<div id="mainPic" class="research">
+								<h1>Research</h1>
+							</div><!-- end #mainPic -->
+							<div id="mainBody"> 
+							
+							<p>Our research on ProveIt is supported by <a href="http://nsf.gov/awardsearch/showAward.do?AwardNumber=0537251">grant #0537251</a> from the National Science Foundation.</p>
+							
+								<h2>Demonstrations</h2>
+								<p>We have demoed ProveIt at the following symposium:</p>
+								
+								<ul>
+									<li>Kurt Luther, Matthew Flaschen, Andrea Forte, Christopher Jordan, and Amy Bruckman, <strong><a href="/pdf/a43-luther.pdf">ProveIt: A New Tool for Supporting Citation in MediaWiki</a></strong>, presented at the 2009 ACM Symposium on Wikis and Open Collaboration (WikiSym '09), Orlando, FL</li>
+								</ul>
+								
+								<h2>Posters</h2>
+								<p>We have done poster presentations of ProveIt at the following symposia:</p>
+								<ul>
+									<li>Matt Flaschen, Christopher Jordan, Kurt Luther, Andrea Forte, and Amy Bruckman, <strong>ProveIt: A New Tool for Reliable Wikipedia References</strong>, presented at the 2009 Georgia Tech Undergraduate Research in Computing (UROC) Symposium, Atlanta, GA</li>
+									<li>Matt Flaschen, Christopher Jordan, Kurt Luther, Andrea Forte, and Amy Bruckman, <strong>ProveIt: A New Tool for Reliable Wikipedia References</strong>, presented at the 2009 Georgia Tech Undergraduate Research (UROP) Spring Symposium, Atlanta, GA</li>
+								</ul>
+   
+							</div><!-- end #mainBody -->
+						</td>
+						<td id="sideTableofContents">&nbsp; </td>
+					</tr>
+				</table> 
+
+
+<?php include_once 'footer.php'; ?>
