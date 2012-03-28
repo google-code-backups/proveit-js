10994c0d34611b685502b55599823bb74100510f
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-03-27 23:42:47 -0400

    Fix issue 134.  Unbind ProveIt click event (using namespace) on acceptButton (only) right before new event is bound.  Remove unnecessary log.

---
+++ website/demo.php
@@ -0,0 +1,114 @@
+<?php
+
+$page = 'Demo';
+include_once 'header.php';
+
+?>
+<link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css" rel="Stylesheet" />
+<script src="wikibits.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/jquery-ui.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+// Mock Wikipedia environment ProveIt expects to run in
+
+// Deliberately global
+wgServer="http://en.wikipedia.org",
+wgScriptPath="/w",
+wgCanonicalNamespace="",
+wgCanonicalSpecialPageName=false,
+wgNamespaceNumber=0,
+wgAction="edit";
+
+var mw =
+{
+	loader:
+	{
+		using: function(dependencies, success)
+		{
+			success();
+		}
+	}
+}
+
+</script>
+<script src="http://en.wikipedia.org/w/resources-1.19/resources/jquery/jquery.textSelection.js" type="text/javascript"></script>
+<script src="http://proveit-js.googlecode.com/hg/ProveIt_Wikipedia.js" type="text/javascript"></script>
+<script type="text/javascript">
+//<![CDATA[
+proveit.loadMaximized = true;
+function loadArticle(articleName)
+{
+  if(articleName == null)
+    articleName = $('#articleName').val();
+  else
+    $('#articleName').val(articleName);
+  
+  $('#articleLink').text(articleName).attr('href', wgServer + '/wiki/' + encodeURIComponent(articleName));
+
+  $('#articleName').attr('readonly','readonly');
+  $('#articleBtn').attr('disabled','disabled');
+  
+  var apiURL = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=' + encodeURIComponent(articleName) + '&rvprop=content&format=json&callback=?';
+  $.getJSON(apiURL, function(response)
+  {
+    if(response.error)
+    {
+      throw response.error; 
+    }
+	else
+	{
+      $('#articleName').removeAttr('readonly');
+      $('#articleBtn').removeAttr('disabled');	  
+	}
+    var pages = response.query.pages;
+    for(var key in pages)
+    {
+      break; // Get first (only) key.  This is necessary due to odd JSON structure.
+    }
+    var page = pages[key];
+    var content = page.revisions[0]['*'];
+    // wg's global 
+    wgTitle = page.title;
+    wgPageName = page.title.replace(" ", "_");
+    $('#wpTextbox1').val(content);
+    $('#proveit').remove();
+    proveit.createGUI();
+  });
+  return false;
+}
+
+$(function()
+{
+    jQuery.getScript(proveit.JQUERYUI_SCRIPT_URL, loadArticle);
+    $('#demoForm').submit(function(evt){
+	            loadArticle();
+		    evt.preventDefault();
+		});
+});
+//]]>
+</script>
+				<table id="mainTable">
+					<tr>
+						<td id="mainContent">
+							<div id="mainBody">
+								<h2>Demo</h2> 
+
+								<p>See that cool-looking gadget in the bottom right corner of this window? <strong>That's ProveIt</strong>, and you can test drive it right here with any Wikipedia article. We've preloaded the article on Georgia Tech by default, but if you want to try a different one, just type the article name into the box below and click "Load article."</p>
+								<form id="demoForm" action="">
+									<fieldset>
+										<label for="articleName">Wikipedia article name:</label>
+										<input id="articleName" size="35" style="width: 300px;" value="Georgia Institute of Technology"/>
+										<input id="articleBtn" type="submit" value="Load article"/>
+										<p style="margin: 10px 0 20px 0;">More suggestions: <a href="#" onclick="loadArticle('Tech Tower')">Tech Tower</a> - <a href="#" onclick="loadArticle('ANAK Society')">ANAK Society</a> - <a href="#" onclick="loadArticle('Ramblin\' Wreck')">Ramblin' Wreck</a></p>
+
+										<label for="wpTextbox1"><a id="articleLink"></a> article from Wikipedia:</label>
+										<textarea rows="25" cols="115" style="width: 100%" id="wpTextbox1"></textarea>
+									</fieldset>
+								</form>
+								<p>Wikipedia article used and made available under the <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution/Share-Alike License 3.0 (Unported)</a>.</p>
+							</div>
+						</td>
+					</tr>
+				</table>
+<?php include_once 'footer.php'; ?>
---
+++ website/reportabug.php
@@ -0,0 +1,69 @@
+<?php
+
+$page = 'Report-a-Bug';
+
+include_once 'header.php';
+
+?>
+
+			<!-- Main Content Table -->
+
+				<table id="mainTable">
+					<tr>
+						<td id="mainContent">
+
+							<div id="mainPic" class="reportabug">
+								<h1>Report a Bug</h1>
+							</div><!-- end #mainPic -->
+							<div id="mainBody"> 
+								<h2 id="GettingStarted">Getting Started</h2>
+								<p>All users are encouraged to report any bugs (or glitches) encountered while using ProveIt. Any and all help is appreciated, and will be used to further improve the ProveIt experience.</p>
+								<!--br/-->
+								
+								<hr />
+								<h2 id="Fields">Issue Tracker</h2>
+								<p>We use Google Code's issue tracker to manage bug reports. To create a new bug report, go to the <a href="http://code.google.com/p/proveit-js/issues/entry?template=Defect%20report%20from%20user">New Issue</a> page. You'll see a number of fields:</p>
+								<dl>
+									<dt><i>Summary</i></dt>
+									<dd>A brief, one-line summary of the problem you're having.</dd>
+									<dt>Description</dt>
+									<dd>A detailed description of your problem. See our <a href="#Tips">tips for writing good bug reports</a> below.</dd>
+									<dt>Status</dt>
+									<dd>The status of the bug. Your report will initially be marked <em>New</em>.  If it is verified, it will be changed to <em>Accepted</em>.  When we have begun working, it will be changed to <em>Started</em>.  Finally, when work is complete, it will be marked <em>Fixed</em>.</dd>
+									<dt>Label(s)</dt>
+									<dd>One or more short tags used to categorize the report. We use various labels help organize reports, including <em>Type</em> (e.g. Defect, Enhancement, or Task), <em>Priority</em>, and <em>OpSys</em> (operating system).</dd>
+									<dt>Owner</dt>
+									<dd>The developer responsible for monitoring your report. You should post any relevant information to the issue page, rather than contacting the developer directly.</dd>
+								</dl>
+								
+								<p>Once you've filled out the Status and Description fields, click the "Submit issue" button. You're done! Thanks in advance.</p>
+								<!--br/-->
+								
+								<hr />
+								<h2 id="Tips">Tips for Writing Bug Reports</h2>
+								<p>To help the ProveIt developers fix the bug, please provide enough information for them to reproduce it. This will typically include:</p>
+								<ul>
+									<li>Your browser and its version.  For example, Firefox 3.6.10, Internet Explorer 8, or Chrome 7.0.517.24</li>
+									<li>The article you were editing</li>
+									<li>The expected result</li>
+									<li>The problem you experienced</li>
+								</ul>								
+								
+								<h2><a href="http://code.google.com/p/proveit-js/issues/entry?template=Defect%20report%20from%20user">Report a Bug Now</a></h2>
+
+							</div><!-- end #mainBody -->
+						</td>
+						<td id="sideTableofContents"> 
+							<h2>Report a Bug </h2>
+							<ul>
+								<li><a href="#GettingStarted">Getting Started</a></li>
+								<li><a href="#Fields">Issue Tracker</a></li>
+								<li><a href="#Tips">Tips for Writing Bug Reports</a></li>
+								<li><a href="http://code.google.com/p/proveit-js/issues/entry?template=Defect%20report%20from%20user">Report a Bug Now</a></li>
+							</ul>
+					  </td>
+					</tr>
+				</table> 
+
+
+<?php include_once 'footer.php'; ?>
